d2d64140868e921c4074a1fbdb53d4ec6f379206
Nima <nimacodes@gmail.com>
2024-04-02 01:42:21 +0330

    Good understanding here. GREAT

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { SignupDTO } from './dto/signup.dto';
 import { UsersService } from './users.service';
+import { UpdateDTO } from './dto/update.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -8,6 +9,26 @@ export class UsersController {
 
 	@Post('signup')
 	createUser(@Body() body: SignupDTO) {
-        this.userService.create(body.email, body.password);
+        return this.userService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.userService.findOne(+id);
+    }
+
+    @Get()
+    findAll(@Query('email') email: string) {
+        return this.userService.find(email ?? email)
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateDTO) {
+        return this.userService.update(+id, body)
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(+id)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -10,6 +10,44 @@ export class UsersService {
 	create(email: string, password: string) {
 		const user = this.repo.create({ email, password });
 
-        return this.repo.save(user);
+		return this.repo.save(user);
+	}
+
+	findOne(id: number) {
+		return this.repo.findOne({ where: { id: id } }); 
+	}
+
+	find(email?: string) {
+		return this.repo.find({ where: { email: email ?? email } });
+	}
+
+	// ----------------Bad----------------
+	// update(id: number, email: string, password: string) {
+	// 	return this.repo.update(id, {
+	// 		email,
+	// 		password,
+	// 	});
+	// }
+	// -----------------------------------
+
+	// acceptable:
+	async update(id: number, attrs: Partial<User>) {
+		// insert & update => Don't run hooks.
+		// save => run hooks. | Trade-off between hooks and performance (double DB call)
+
+		const user = await this.findOne(id);
+		if (!user) throw new BadRequestException('User with the given ID was not found!');
+
+		Object.assign(user, attrs);
+
+		return this.repo.save(user);
+	}
+
+	// unlike delete, *remove* will call hooks. | trade-off again.
+	async remove(id: number) {
+		const user = await this.findOne(id);
+		if (!user) return new BadRequestException('User with the given ID was not found!');
+
+		return this.repo.remove(user);
 	}
 }
